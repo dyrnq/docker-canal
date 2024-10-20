cce31b8bece130c4f461194876ee9e74d2ad7ecf
dyrnq <dyrnq@qq.com>
2024-10-20 13:05:52 +0800

    usage-demo/k8s use env update canal.properties

--- usage-demo/k8s/check.sh
+++ usage-demo/k8s/check.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 
+kubectl exec -it canal-server-0 -- bash -c "env |grep ^canal"
 kubectl exec -it canal-server-0 -- bash -c "apt update;apt install netcat-openbsd inetutils-ping;"
 kubectl exec -it canal-server-0 -- bash -c "ping canal-server-1.canal-server-headless.default.svc.cluster.local"
 kubectl exec -it canal-server-0 -- bash -c "nc -vz canal-server-1.canal-server-headless.default.svc.cluster.local 11110"
--- usage-demo/k8s/launch.sh
+++ usage-demo/k8s/launch.sh
@@ -5,10 +5,7 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd -P)
 kubectl apply -f https://ghp.ci/https://github.com/dyrnq/dist/raw/main/zookeeper/latest/10-deployments.yaml
 
 
-sed \
--e "s@__CANAL_ZK__@zookeeper-0.zookeeper-headless.default.svc.cluster.local:2181,zookeeper-1.zookeeper-headless.default.svc.cluster.local:2181,zookeeper-2.zookeeper-headless.default.svc.cluster.local:2181@g" \
-"${SCRIPT_DIR}/deployments.yaml" | \
-kubectl apply -f -
+kubectl apply -f ${SCRIPT_DIR}/deployments.yaml
 
 
 # kubectl logs deployments/canal-server
