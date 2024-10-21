6bbdf7654b4027821ef2e067a6e3ffc054d94e2a
dyrnq <dyrnq@qq.com>
2024-10-21 11:53:17 +0800

    Update check.sh

--- usage-demo/k8s/check.sh
+++ usage-demo/k8s/check.sh
@@ -2,8 +2,8 @@
 
 
 kubectl exec -it canal-server-0 -- bash -c "env |grep ^canal"
-kubectl exec -it canal-server-0 -- bash -c "apt update;apt install netcat-openbsd inetutils-ping;"
-kubectl exec -it canal-server-0 -- bash -c "ping canal-server-1.canal-server-headless.default.svc.cluster.local"
+kubectl exec -it canal-server-0 -- bash -c "apt update;apt install netcat-openbsd inetutils-ping -y;"
+kubectl exec -it canal-server-0 -- bash -c "ping -c 3 canal-server-1.canal-server-headless.default.svc.cluster.local"
 kubectl exec -it canal-server-0 -- bash -c "nc -vz canal-server-1.canal-server-headless.default.svc.cluster.local 11110"
 
 
