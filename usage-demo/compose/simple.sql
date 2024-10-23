fc7e243402f3999aca26dce0ab2266646e79e9f8
dyrnq <dyrnq@qq.com>
2024-10-23 11:02:39 +0800

    Add usage-demo/compose/simple.sql

---
+++ usage-demo/compose/simple.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `person`;
+CREATE TABLE `person` (
+  `id` int DEFAULT NULL,
+  `last_name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin DEFAULT NULL,
+  `first_name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin DEFAULT NULL,
+  `city` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+
+
+insert into `person` (`id`, `last_name`, `first_name`, `address`, `city`) values (1, 'Li', 'Lily', '123 Main St', 'New York');
