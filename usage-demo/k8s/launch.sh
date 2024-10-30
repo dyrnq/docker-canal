b2eb89433499a20281eef3861587281c337f5a6b
dyrnq <dyrnq@qq.com>
2024-10-30 14:49:59 +0800

    Add k8s deployments with canal-admin.

--- usage-demo/k8s/launch.sh
+++ usage-demo/k8s/launch.sh
@@ -5,10 +5,11 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd -P)
 
 
 remove_flag=""
+canal_admin=""
 while [ $# -gt 0 ]; do
     case "$1" in
-        --detached|-d)
-            DETACHED=1
+        --with-admin|-a)
+            canal_admin="1";
             ;;
          --remove|-r)
             remove_flag="1";
@@ -27,9 +28,18 @@ done
 if [ "$remove_flag" = "" ]; then
     kubectl apply -f https://ghp.ci/https://github.com/dyrnq/dist/raw/main/zookeeper/latest/10-deployments.yaml
     kubectl apply -f ${SCRIPT_DIR}/deployments.yaml
+
+    if [ "$canal_admin" = "1" ]; then
+        kubectl apply -f ${SCRIPT_DIR}/optional-canal-admin.yaml
+    else
+        kubectl apply -f ${SCRIPT_DIR}/optional-configmap.yaml
+    fi
+
 else
     kubectl delete -f https://ghp.ci/https://github.com/dyrnq/dist/raw/main/zookeeper/latest/10-deployments.yaml
     kubectl delete -f ${SCRIPT_DIR}/deployments.yaml
+    kubectl delete -f ${SCRIPT_DIR}/optional-canal-admin.yaml
+    kubectl delete -f ${SCRIPT_DIR}/optional-configmap.yaml
 fi
 
 
